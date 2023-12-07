5dba8896683f1a38202421905ee466229bd0817a
aulanbekuulu <aulanbekuulu@srs.lan>
2023-12-07 13:39:25 +0600

    Переиспользуемый компонент List. Generics, Обобщенные типы в typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./Components/Card";
 import UserList from "./Components/UserList";
-import {IUser} from "./types/types";
+import {ITodo, IUser} from "./types/types";
 import axios from "axios";
+import List from "./Components/List";
+import UserItem from "./Components/UserItem";
+import TodoItem from "./Components/TodoItem";
 
 const App = () => {
     const [users,setUsers] = useState<IUser[]>([])
+    const [todos,setTodos] = useState<ITodo[]>([])
     useEffect(() => {
         fetchUsers()
+        fetchTodos()
     }, [])
 
     async function fetchUsers(){
@@ -18,12 +23,29 @@ const App = () => {
             alert(e)
         }
     }
+
+    async function fetchTodos(){
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
         <div>
             <Card width='200px' height='200px' variant={CardVariant.primary} onClick={(num) => console.log('click', num)}>
                 <button>Кнопка</button>
             </Card>
-            <UserList users={users}/>
+            <List items={users}
+                  renderItem={
+                (user: IUser) => <UserItem user={user} key={user.id}/>
+            }/>
+            <List items={todos}
+                  renderItem={
+                (todo: ITodo) => <TodoItem todo={todo} key={todo.id}/>
+            }/>
         </div>
     );
 };
