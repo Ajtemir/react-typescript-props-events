62e896bdd3325d5d47a744366f563d11d4f4c757
aulanbekuulu <aulanbekuulu@srs.lan>
2023-12-07 13:21:10 +0600

    Типизация запроса axios. Типизация хука UseState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./Components/Card";
 import UserList from "./Components/UserList";
 import {IUser} from "./types/types";
+import axios from "axios";
 
 const App = () => {
-    const users: IUser[] = [
-        {id: 1, name: 'Ulbi tv', email: 'asf@mail.ru', address: {city:'Moscow', street:'Lenin', zipcode:'12'},},
-        {id: 2, name: 'Timur', email: 'asf@mail.ru', address: {city:'Maliningrad', street:'Pushkin', zipcode:'12'},},
-    ]
+    const [users,setUsers] = useState<IUser[]>([])
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    async function fetchUsers(){
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
     return (
         <div>
             <Card width='200px' height='200px' variant={CardVariant.primary} onClick={(num) => console.log('click', num)}>
