b7a85ae2a7ed3abf1704a4fe61b6c5f1f9eaf22b
aulanbekuulu <aulanbekuulu@srs.lan>
2023-12-07 13:05:00 +0600

    Компонент UserList. IUser, IAddress, UserItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Card, {CardVariant} from "./Components/Card";
+import UserList from "./Components/UserList";
+import {IUser} from "./types/types";
 
 const App = () => {
+    const users: IUser[] = [
+        {id: 1, name: 'Ulbi tv', email: 'asf@mail.ru', address: {city:'Moscow', street:'Lenin', zipcode:'12'},},
+        {id: 2, name: 'Timur', email: 'asf@mail.ru', address: {city:'Maliningrad', street:'Pushkin', zipcode:'12'},},
+    ]
     return (
         <div>
             <Card width='200px' height='200px' variant={CardVariant.primary} onClick={(num) => console.log('click', num)}>
                 <button>Кнопка</button>
             </Card>
+            <UserList users={users}/>
         </div>
     );
 };
